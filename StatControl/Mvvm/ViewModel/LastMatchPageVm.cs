9da9705feaa5c9574243bb2dedf1ff6051dcd902
Kieran Davies <kieran_davies@yahoo.co.uk>
2021-12-16 10:13:55 +0000

    Updated NuGet Packages

--- StatControl/Mvvm/ViewModel/LastMatchPageVm.cs
+++ StatControl/Mvvm/ViewModel/LastMatchPageVm.cs
@@ -1,16 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
-using StatControl.Mvvm.View;
-using FunctionZero.CommandZero;
-using FunctionZero.MvvmZero;
+﻿using FunctionZero.MvvmZero;
 using System.Windows.Input;
 using System.Threading.Tasks;
+using StatControl.Services;
+using System.Diagnostics;
+using Xamarin.Forms;
+using StatControl.Mvvm.Model.SteamUserProfile;
+using StatControl.Mvvm.Model.SteamUserAchievements;
+using StatControl.Mvvm.Model.SteamGameStats;
+using System.ComponentModel;
 
 namespace StatControl.Mvvm.ViewModel
 {
     internal class LastMatchPageVm : MvvmZeroBaseVm
     {
+        private SteamGameStatsResponse _resultStats;
+        
+        public SteamGameStatsResponse ResultStats
+        {
+            get => _resultStats;
+            set
+            {
+                SetProperty(ref _resultStats, value);
+                OnPropertyChanged();
+            }
+        }
+
+        public LastMatchPageVm()
+        {
+            MessagingCenter.Subscribe<CarouselPageVm, SteamGameStatsResponse>(this, "resultStats", (sender, resultStats) =>
+            {
+                Debug.WriteLine("Received Lastmatch Fun");
+                ResultStats = resultStats;
+            });
+        }
     }
 }
