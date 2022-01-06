dc5141b1174d855d171ab38d0175148f9dce382f
JECartwright <94446728+JECartwright@users.noreply.github.com>
2022-01-06 14:01:51 +0000

    Added Messaging Center

--- StatControl/Mvvm/ViewModel/IndividualWeaponPageVm.cs
+++ StatControl/Mvvm/ViewModel/IndividualWeaponPageVm.cs
@@ -8,14 +8,15 @@ using FunctionZero.MvvmZero;
 using System.Windows.Input;
 using System.Threading.Tasks;
 using StatControl.Mvvm.Model.DisplayModel;
+using StatControl.Mvvm.Model.SteamGameStats;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace StatControl.Mvvm.ViewModel
 {
     internal class IndividualWeaponPageVm : MvvmZeroBaseVm
     {
-
         private readonly Dictionary<String, String> _favWeaponDictionary = new Dictionary<String, String>()
             {
                 { "deagle", "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/weapons/base_weapons/weapon_deagle.29e8f0d7d0be5e737d4f663ee8b394b5c9e00bdd.png" },
@@ -52,6 +53,10 @@ namespace StatControl.Mvvm.ViewModel
                 { "hegrenade", "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/weapons/base_weapons/weapon_hegrenade.7b344756d5dbdda4fd2e583a227a670599889f59.png" }
             };
 
+        //geter seters
+
+        private SteamGameStatsResponse _resultStats;
+
         public string CurrentWeapon
         {
             get => Preferences.Get(nameof(CurrentWeapon), null);
@@ -70,23 +75,572 @@ namespace StatControl.Mvvm.ViewModel
             set => SetProperty(ref _weaponImage, value);
         }
 
-        public void PageSettup()
+        private Color _customRed = new Color(255, 0, 0);
+        private Color _customGreen = new Color(0, 255, 0);
+
+        private string _rangeTimeframe;
+        public string RangeTimeframe
         {
+            get => _rangeTimeframe;
+            set
+            {
+                value = "Changes Over " + value;
+                SetProperty(ref _rangeTimeframe, value);
+            }
+        }
+
+        private string _rangeAccuracy;
+        public string RangeAccuracy
+        {
+            get => _rangeAccuracy;
+            set
+            {
+                value = "Accuracy: " + value + "%";
+                SetProperty(ref _rangeAccuracy, value);
+            }
+        }
+
+        private string _rangeKills;
+        public string RangeKills
+        {
+            get => _rangeKills;
+            set
+            {
+                value = "Kills: " + value;
+                SetProperty(ref _rangeKills, value);
+            }
+        }
+
+        private string _rangeHits;
+        public string RangeHits
+        {
+            get => _rangeHits;
+            set
+            {
+                value = "Hits: " + value;
+                SetProperty(ref _rangeHits, value);
+            }
+        }
+
+        private string _rangeShots;
+        public string RangeShots
+        {
+            get => _rangeShots;
+            set
+            {
+                value = "Shots: " + value;
+                SetProperty(ref _rangeShots, value);
+            }
+        }
+
+        private string _rangeMisses;
+        public string RangeMisses
+        {
+            get => _rangeMisses;
+            set
+            {
+                value = "Misses: " + value;
+                SetProperty(ref _rangeMisses, value);
+            }
+        }
+
+        private string _globalAccuracy;
+        public string GlobalAccuracy
+        {
+            get => _globalAccuracy;
+            set
+            {
+                value = "Accuracy: " + value + "%";
+                SetProperty(ref _globalAccuracy, value);
+            }
+        }
+
+        private string _globalKills;
+        public string GlobalKills
+        {
+            get => _globalKills;
+            set
+            {
+                value = "Kills: " + value;
+                SetProperty(ref _globalKills, value);
+            }
+        }
+
+        private string _globalHits;
+        public string GlobalHits
+        {
+            get => _globalHits;
+            set
+            {
+                value = "Hits: " + value;
+                SetProperty(ref _globalHits, value);
+            }
+        }
+
+        private string _globalShots;
+        public string GlobalShots
+        {
+            get => _globalShots;
+            set
+            {
+                value = "Shots: " + value;
+                SetProperty(ref _globalShots, value);
+            }
+        }
+
+        private string _globalMisses;
+        public string GlobalMisses
+        {
+            get => _globalMisses;
+            set
+            {
+                value = "Misses: " + value;
+                SetProperty(ref _globalMisses, value);
+            }
+        }
+
+        private string _rangeAccuracyChange;
+        public string RangeAccuracyChange
+        {
+            get => _rangeAccuracyChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _rangeAccuracyChangeColor = _customRed;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _rangeAccuracyChangeColor = _customGreen;
+                    }
+                }                
+                SetProperty(ref _rangeAccuracyChange, value);
+            }
+        }
+
+        private string _rangeKillsChange;
+        public string RangeKillsChange
+        {
+            get => _rangeKillsChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _rangeKillsChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _rangeKillsChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _rangeKillsChange, value);
+            }
+        }
+
+        private string _rangeHitsChange;
+        public string RangeHitsChange
+        {
+            get => _rangeHitsChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _rangeHitsChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _rangeHitsChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _rangeHitsChange, value);
+            }
+        }
+
+        private string _rangeShotsChange;
+        public string RangeShotsChange
+        {
+            get => _rangeShotsChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _rangeShotsChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _rangeShotsChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _rangeShotsChange, value);
+            }
+        }
+
+        private string _rangeMissesChange;
+        public string RangeMissesChange
+        {
+            get => _rangeMissesChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _rangeMissesChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _rangeMissesChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _rangeMissesChange, value);
+            }
+        }
+
+        private string _globalAccuracyChange;
+        public string GlobalAccuracyChange
+        {
+            get => _globalAccuracyChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _globalAccuracyChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _globalAccuracyChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _globalAccuracyChange, value);
+            }
+        }
+
+        private string _globalKillsChange;
+        public string GlobalKillsChange
+        {
+            get => _globalKillsChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _globalKillsChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _globalKillsChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _globalKillsChange, value);
+            }
+        }
+
+        private string _globalHitsChange;
+        public string GlobalHitsChange
+        {
+            get => _globalHitsChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _globalHitsChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _globalHitsChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _globalHitsChange, value);
+            }
+        }
+
+        private string _globalShotsChange;
+        public string GlobalShotsChange
+        {
+            get => _globalShotsChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _globalShotsChangeColor = _customGreen;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _globalShotsChangeColor = _customRed;
+                    }
+                }                
+                SetProperty(ref _globalShotsChange, value);
+            }
+        }
+
+        private string _globalMissesChange;
+        public string GlobalMissesChange
+        {
+            get => _globalMissesChange;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (Convert.ToSingle(value) > 0)
+                    {
+                        value = "↑" + value + "%";
+                        _globalMissesChangeColor = _customRed;
+                    }
+                    else
+                    {
+                        value = "↓" + value + "%";
+                        _globalMissesChangeColor = _customGreen;
+                    }
+                }                
+                SetProperty(ref _globalMissesChange, value);
+            }
+        }
+
+        private Color _rangeAccuracyChangeColor;
+        public Color RangeAccuracyChangeColor
+        {
+            get => _rangeAccuracyChangeColor;
+            set
+            {
+                SetProperty(ref _rangeAccuracyChangeColor, value);
+            }
+        }
+
+        private Color _rangeKillsChangeColor;
+        public Color RangeKillsChangeColor
+        {
+            get => _rangeKillsChangeColor;
+            set
+            {
+                SetProperty(ref _rangeKillsChangeColor, value);
+            }
+        }
+
+        private Color _rangeHitsChangeColor;
+        public Color RangeHitsChangeColor
+        {
+            get => _rangeHitsChangeColor;
+            set
+            {
+                SetProperty(ref _rangeHitsChangeColor, value);
+            }
+        }
+
+        private Color _rangeShotsChangeColor;
+        public Color RangeShotsChangeColor
+        {
+            get => _rangeShotsChangeColor;
+            set
+            {
+                SetProperty(ref _rangeShotsChangeColor, value);
+            }
+        }
+
+        private Color _rangeMissesChangeColor;
+        public Color RangeMissesChangeColor
+        {
+            get => _rangeMissesChangeColor;
+            set
+            {
+                SetProperty(ref _rangeMissesChangeColor, value);
+            }
+        }
+
+        private Color _globalAccuracyChangeColor;
+        public Color GlobalAccuracyChangeColor
+        {
+            get => _globalAccuracyChangeColor;
+            set
+            {
+                SetProperty(ref _globalAccuracyChangeColor, value);
+            }
+        }
+
+        private Color _globalKillsChangeColor;
+        public Color GlobalKillsChangeColor
+        {
+            get => _globalKillsChangeColor;
+            set
+            {
+                SetProperty(ref _globalKillsChangeColor, value);
+            }
+        }
+
+        private Color _globalHitsChangeColor;
+        public Color GlobalHitsChangeColor
+        {
+            get => _globalHitsChangeColor;
+            set
+            {
+                SetProperty(ref _globalHitsChangeColor, value);
+            }
+        }
+
+        private Color _globalShotsChangeColor;
+        public Color GlobalShotsChangeColor
+        {
+            get => _globalShotsChangeColor;
+            set
+            {
+                SetProperty(ref _globalShotsChangeColor, value);
+            }
+        }
+
+        private Color _globalMissesChangeColor;
+        public Color GlobalMissesChangeColor
+        {
+            get => _globalMissesChangeColor;
+            set
+            {
+                SetProperty(ref _globalMissesChangeColor, value);
+            }
+        }
+
+        private string _score;
+        public string Score
+        {
+            get => _score;
+            set
+            {
+                value = "Score: " + value;
+                SetProperty(ref _score, value);
+            }
+        }
+
+        public string WeaponDataShots;
+        public string WeaponDataHits;
+        public string WeaponDataKills;
+
+        public SteamGameStatsResponse ResultStats
+        {
+            get => _resultStats;
+            set
+            {
+                //_resultStats = value;
+                SetProperty(ref _resultStats, value);                
+            }
+        }
+
+        //fuctions
+
+        public void PageSettup(SteamGameStatsResponse data)
+        {            
             Weapon = CurrentWeapon;
             WeaponImage = _favWeaponDictionary[Weapon];
+            WeaponDataShots = data.playerstats.stats.Find(x => x.name.Equals($"total_shots_{Weapon}"))?.value.ToString() ?? "0";
+            WeaponDataHits = data.playerstats.stats.Find(x => x.name.Equals($"total_hits_{Weapon}"))?.value.ToString() ?? "0";
+            WeaponDataKills = data.playerstats.stats.Find(x => x.name.Equals($"total_kills_{Weapon}"))?.value.ToString() ?? "0";
+            SetTrueValues();
             OnPropertyChanged();
         }
 
         public void GetWeaponAsync()
         {
+            MessagingCenter.Subscribe<CarouselPageVm, SteamGameStatsResponse>(this, "resultStats", (sender, resultStats) =>
+            {
+                Debug.WriteLine("Weapon Messaging Compleate");
+                ResultStats = resultStats;
+                //_resultStats = resultStats;
+                Debug.WriteLine("test");
+            });
             while (true)
             {
                 if (this.IsOwnerPageVisible)
                 {
-                    PageSettup();                    
+                    PageSettup(_resultStats);                    
                     break;
                 }
             }           
+        }
+
+        public void SetDefaultValues()
+        {
+            RangeTimeframe = "2 Weeks";
+            RangeKills = "0";
+            RangeKillsChange = "0";
+            RangeHits = "0";
+            RangeHitsChange = "0";
+            RangeShots = "0";
+            RangeShotsChange = "0";
+            RangeAccuracy = "0";
+            RangeAccuracyChange = "0";
+            RangeMisses = "0";
+            RangeMissesChange = "0";
+            GlobalKills = "0";
+            GlobalKillsChange = "0";
+            GlobalHits = "0";
+            GlobalHitsChange = "0";
+            GlobalShots = "0";
+            GlobalShotsChange = "0";
+            GlobalAccuracy = "0";
+            GlobalAccuracyChange = "0";
+            GlobalMisses = "0";
+            GlobalMissesChange = "0";
+            Score = "0";
+            OnPropertyChanged();
+        }
+
+        public void SetTrueValues()
+        {
+            int weaponshots = 0;
+            int weaponhits = 0;
+            int weaponkills = 0;
+            int weaponmisses = 0;
+            float weaponaccuracy = 0f;
+            if (!string.IsNullOrEmpty(WeaponDataShots))
+            {
+                weaponshots = Convert.ToInt32(WeaponDataShots);
+            }
+            if (!string.IsNullOrEmpty(WeaponDataHits))
+            {
+                weaponhits = Convert.ToInt32(WeaponDataHits);
+            }
+            if (!string.IsNullOrEmpty(WeaponDataKills))
+            {
+                weaponkills = Convert.ToInt32(WeaponDataKills);
+            }
+            weaponmisses = weaponshots - weaponhits;
+            weaponaccuracy = Convert.ToSingle(weaponhits) / Convert.ToSingle(weaponshots);
+            string weaponaccuracypercent = (Math.Round(weaponaccuracy, 2) * 100).ToString();
+            GlobalAccuracy = weaponaccuracypercent;
+            GlobalMisses = weaponmisses.ToString();
+            GlobalHits = weaponhits.ToString();
+            GlobalKills = weaponkills.ToString();
+            GlobalShots = weaponshots.ToString();
+
         }
 
         public IndividualWeaponPageVm()
@@ -95,6 +649,7 @@ namespace StatControl.Mvvm.ViewModel
             {
                 GetWeaponAsync();
             });
+            SetDefaultValues();            
         }
     }
 }
