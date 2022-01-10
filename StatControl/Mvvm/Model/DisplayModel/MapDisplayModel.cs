f11fea7b75054ee902903f31e396f945f3bb2480
Kieran Davies <kieran_davies@yahoo.co.uk>
2022-01-10 12:55:10 +0000

    Maps Page Complete

--- StatControl/Mvvm/Model/DisplayModel/MapDisplayModel.cs
+++ StatControl/Mvvm/Model/DisplayModel/MapDisplayModel.cs
@@ -9,12 +9,14 @@ namespace StatControl.Mvvm.Model.DisplayModel
         public string ApiName { get; set; }
         public string TotalRounds { get; set; }
         public string TotalRoundsWon { get; set; }
+        public string MapImage { get; set; }
 
-        public MapDisplayModel(String apiName, int totalRounds, int totalRoundsWon)
+        public MapDisplayModel(String apiName, int totalRounds, int totalRoundsWon, string mapImage)
         {
             ApiName = apiName;
             TotalRounds = totalRounds.ToString();
             TotalRoundsWon = totalRoundsWon.ToString();
+            MapImage = mapImage;
         }
     }
 }
